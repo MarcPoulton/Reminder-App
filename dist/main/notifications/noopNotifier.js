b6f80ec6c09e040e6c9448acc94ef259d87b882c
MarcPoulton <153561460+MarcPoulton@users.noreply.github.com>
2026-05-27 12:08:51 +0100

    Delete dist directory

--- dist/main/notifications/noopNotifier.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.noopNotifier = void 0;
-exports.noopNotifier = {
-    async notifyReminderFired(_reminder) {
-        // Intentionally no-op for task 3.
-    },
-};
-//# sourceMappingURL=noopNotifier.js.map
